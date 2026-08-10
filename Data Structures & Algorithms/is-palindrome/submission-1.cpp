7cd0912c7438351482320077a4c0de114c5d3adc
Vasil Dimitrov <137104869+Voinki@users.noreply.github.com>
2026-08-10 18:41:37 +0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s.empty()) return true;
+
+        int first = 0;
+        int last = s.size() -1;
+
+        while (first < last) {
+            while (first < last && !isalnum(s[first])) first++;
+
+            while (first < last && !isalnum(s[last])) last--;
+            
+            if (tolower(s[first]) != tolower(s[last])) return false;
+
+            first++, last--;
+        }
+
+        return true;
+    }
+};
